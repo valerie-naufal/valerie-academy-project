b1dbe8377414a299895b828a117562243f3186b9
valerie-naufal <valerie.naufal@gmail.com>
2025-03-09 18:17:16 +0200

    Button Component

---
+++ src/app/shared/button/button.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports:[CommonModule]
+})
+export class ButtonComponent {
+  @Input() size: 'L' | 'S' | 'M' = 'S';
+  @Input() btnText: string ='';
+}
