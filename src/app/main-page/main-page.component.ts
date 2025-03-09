08f93d6d2077339e3abd3ad61d267166efa26cf6
valerie-naufal <valerie.naufal@gmail.com>
2025-03-09 12:23:54 +0200

    Main page fixes - layout complete

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../core/navbar/navbar.component";
+import { ProductsComponent } from "../products/products.component";
+import { FooterComponent } from '../core/footer/footer.component';
 
 @Component({
   selector: 'app-main-page',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, ProductsComponent,FooterComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
