980830a36e701344388104dcf49bb34241a360ba
valerie-naufal <valerie.naufal@gmail.com>
2025-03-09 12:26:02 +0200

    Global styles added - routes added

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideRouter, Routes } from '@angular/router';
+import { provideRouter, Routes, RouterModule } from '@angular/router';
 import {
   provideHttpClient,
   withInterceptorsFromDi,
@@ -12,18 +12,21 @@ import { GuestGuard } from './app/guards/guest.guard';
 import { LoginComponent } from './app/core/login/login.component';
 import { ProductsComponent } from './app/products/products.component';
 import { MainPageComponent } from './app/main-page/main-page.component';
+import { ProductDetailsComponent } from './app/product-details/product-details.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent, canActivate: [GuestGuard] },
   { path: 'products', component: ProductsComponent, canActivate: [AuthGuard] },
+  { path: 'product/:id', component: ProductDetailsComponent, canActivate: [AuthGuard] },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'main', component: MainPageComponent, canActivate: [AuthGuard] },
 ];
+console.log('Registered Routes:', routes);
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
-    importProvidersFrom(NgxPermissionsModule.forRoot()), 
+    importProvidersFrom(NgxPermissionsModule.forRoot(), RouterModule), 
   ],
 }).catch((err) => console.error(err));
