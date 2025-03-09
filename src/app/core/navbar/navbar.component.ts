cbe3ee374e50a3b91fdf30128a03092ce7dd1a52
valerie-naufal <valerie.naufal@gmail.com>
2025-03-09 14:10:00 +0200

    Navbar Added Search

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  isSearchOpen = false;
+  isScrolled = false;
+  openSearch() {
+    this.isSearchOpen = !this.isSearchOpen;
+  }
 
+  closeSearch() {
+    this.isSearchOpen = false;
+  }
+
+  @HostListener('window:scroll', [])
+  onScroll() {
+    const targetSection = document.getElementById('all-products');
+
+    if (targetSection) {
+      const sectionTop = targetSection.getBoundingClientRect().top;
+      this.isScrolled = sectionTop <= 0; 
+    }
+  }
 }
