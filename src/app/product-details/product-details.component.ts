296e7ac7aca1a9e233f249037c262a02f66b45c2
valerie-naufal <valerie.naufal@gmail.com>
2025-03-09 12:21:26 +0200

    Added Single Product View

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from "../core/navbar/navbar.component";
+import { FooterComponent } from '../core/footer/footer.component';
+
+@Component({
+  selector: 'app-product-details',
+  standalone: true,
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.scss'],
+  imports: [CommonModule, NavbarComponent,FooterComponent],
+})
+export class ProductDetailsComponent implements OnInit {
+  product: any;
+
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
+
+  ngOnInit() {
+    const productId = this.route.snapshot.paramMap.get('id'); 
+
+    console.log(
+      `ProductDetailsComponent: Loading product with ID = ${productId}`
+    );
+
+    if (productId) {
+      this.http
+        .get(`https://fakestoreapi.com/products/${productId}`)
+        .subscribe({
+          next: (data) => {
+            this.product = data;
+            console.log('Product loaded:', this.product);
+          },
+          error: (error) => {
+            console.error('Error fetching product:', error);
+          },
+        });
+    }
+  }
+}
